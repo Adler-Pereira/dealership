2cc1332842648b4e2a224b8dbffc55e0ffee52b2
Adler-Pereira <adlerlimap@gmail.com>
2025-09-14 23:18:20 -0300

    test: comment tests annotation

--- src/test/java/com/crudcar/dealership/DealershipApplicationTests.java
+++ src/test/java/com/crudcar/dealership/DealershipApplicationTests.java
@@ -1,12 +1,12 @@
 package com.crudcar.dealership;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class DealershipApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
